250bcc0d88f6c59e81fad5cf95d19dc3b41ebf25
Oliwier <oliwierrosiak07@gmail.com>
2024-08-07 23:30:01 +0200

    Dodanie przekierowania

--- src/components/linkPage/likePage.js
+++ src/components/linkPage/likePage.js
@@ -2,13 +2,42 @@ import { useEffect, useState } from 'react'
 import styles from './linkPage.module.css'
 import img from '../../assets/img/shortifyLogo.png'
 import LoadingIcon from '../../assets/svg/loadingIcon'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
+import serverAddress from '../serverAddress'
 
 function LinkPage()
 {
     let [dots,setDots] = useState("")
+    const [error,setError] = useState('')
+    
+    const navigation = useNavigate()
+
+    const link = useParams()
+
+    const forwarding = async()=>
+    {
+        try
+        {
+            const response = await axios.get(`${serverAddress}/${link.id}.json`)
+            const usersLink = (Object.values(response.data))[0].link
+            location.href = usersLink
+        }
+        catch(ex)
+        {
+            console.log(ex)
+        }
+    }
 
 
     useEffect(()=>{
+        forwarding()
+
+    },[])
+
+    useEffect(()=>{
+
+
         setTimeout(() => {
             setDots(dots+=".")
             if(dots.length == 4)
