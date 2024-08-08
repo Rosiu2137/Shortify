1320a76c9c4b674449e018c1d4c1dd467dfbed21
Oliwier <oliwierrosiak07@gmail.com>
2024-08-08 23:03:41 +0200

    link succes fixed

--- src/components/linkSucces/linkSucces.js
+++ src/components/linkSucces/linkSucces.js
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from './linkSuccess.module.css'
 import Copy2Icon from '../../assets/svg/copy2Icon'
 
-let timeout1,timeout2,timeout3,timeout4
+let timeout1,timeout2,timeout3,timeout4,timeout5,timeout6 
 
 function LinkSuccess(props)
 {
@@ -11,6 +11,9 @@ function LinkSuccess(props)
     const h2ref = useRef()
     const pRef = useRef()
     const buttonRef = useRef()
+    const infoRef = useRef()
+
+    const [displayInfo,setDisplayInfo] = useState(false)
 
     useEffect(()=>{
         timeout1 = setTimeout(()=>{
@@ -32,6 +35,8 @@ function LinkSuccess(props)
             clearTimeout(timeout2)
             clearTimeout(timeout3)
             clearTimeout(timeout4)
+            clearTimeout(timeout5)
+            clearTimeout(timeout6)
         }
     },[])
 
@@ -39,16 +44,46 @@ function LinkSuccess(props)
     {
 
         navigator.clipboard.writeText(props.shortedLink)
-        alert("Tekst skopiowany")
+        setDisplayInfo(true)
         
     }
+
+
+    useEffect(()=>{
+
+        timeout6 = setTimeout(()=>{
+            if(infoRef.current)
+            {
+                infoRef.current.classList.add(styles.animation)
+
+            }
+        },1500)
+
+        timeout5 = setTimeout(() => {
+            setDisplayInfo(false)
+            if(infoRef.current)
+            {
+                infoRef.current.classList.remove(styles.animation)
+
+            }
+        }, 2000);
+
+    },[displayInfo])
 
     return(
         <article className={styles.article}>
 
             <h1 ref={h1ref}>Oto twój skrócony Link</h1>
 
-            <h2 ref={h2ref}>{props.shortedLink}<button className={styles.autoCopy} onClick={copyAddress}><Copy2Icon cl={styles.icon}/><span className={styles.info}>Skopiowano!</span></button></h2>
+            <h2 ref={h2ref}>{props.shortedLink}
+                <button className={styles.autoCopy} onClick={copyAddress}>
+                    <Copy2Icon cl={styles.icon}/>
+                    {displayInfo?<span className={styles.info} ref={infoRef}>
+                        Skopiowano!
+                        <span></span>
+                    </span>:null}
+                </button>
+            </h2>
 
             <p ref={pRef}>Skopiuj go i wklejaj aby łatwiej dostawać się do swoich ulubionych stron</p>
 
